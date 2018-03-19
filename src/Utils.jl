ec7db2d737507211d38c5b6d4d6ad51475e6c4da
Bill Gross <bill.gross@me.com>
2018-03-19 12:24:56 -0500

    updated to module

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,4 +1,20 @@
+module Utils
+
+
+## DATETIME
+
 using TimeZones
+
+mstodatetime(ms::Int) = Dates.DateTime(Dates.UTInstant(Dates.Millisecond(ms)))
+daystodate(days::Int) = Dates.Date(Dates.UTInstant(Dates.Day(days)))
+unixtime(seconds::Int) = DateTime(1970) + Dates.Second(seconds)
+unixtimems(ms::Int) = DateTime(1970) + Dates.Millisecond(ms)
+
+function tzconvert(datetime::DateTime,from::String,to::String)
+    DateTime(astimezone(ZonedDateTime(datetime,TimeZone(from)),TimeZone(to)))
+end
+
+## CONTROL / OUTPUT
 
 function lock_file(func::Function,fname::AbstractString;timeout=30)
     wait_begin = now()
@@ -12,52 +28,11 @@ function lock_file(func::Function,fname::AbstractString;timeout=30)
         open(lock_fname,"w") do f
             write(f,getpid())
         end
-        try
-            func()
-        finally
-            rm(lock_fname)
-        end
+        func()
+
+        isfile(lock_file) && rm(lock_fname)
     end
 end
-
-mstodatetime(ms::Int) = Dates.DateTime(Dates.UTInstant(Dates.Millisecond(ms)))
-daystodate(days::Int) = Dates.Date(Dates.UTInstant(Dates.Day(days)))
-unixtime(seconds::Int) = DateTime(1970) + Dates.Second(seconds)
-unixtimems(ms::Int) = DateTime(1970) + Dates.Millisecond(ms)
-
-function tzconvert(datetime::DateTime,from::String,to::String)
-    DateTime(astimezone(ZonedDateTime(datetime,TimeZone(from)),TimeZone(to)))
-end
-
-function suppress_stderr(f)
-    old_stderr = STDERR
-    o = redirect_stderr(open("/dev/null","w"))
-    f()
-    redirect_stderr(old_stderr)
-end
-
-z(p,b) = (p-mean(b))/std(b)
-z(p) = (p-mean(p))/std(p)
-
-polyparams{T<:Real}(x::AbstractArray{T,1},n::Integer) = [ float(x[i])^p for i = 1:length(x), p = 0:n ]
-polyfit{T<:Real}(y::AbstractArray{T,1},n::Integer) = polyparams(collect(1:length(y)),n) \ y
-polyfitline(len::Integer,params::Array{Float64,1}) = polyparams(collect(1:len),length(params)-1) * params''
-polyeval(x::Real,params::Array{Float64,1}) = [x^p for p=0:length(params)-1]' * params''
-
-#int_match = r"^(-|\+)?[0-9]+$"
-#float_match = r"^(-|\+)?[0-9]+\.([0-9]+)?(e[0-9]+)?$"
-
-function smartparse(something)
-#    ismatch(int_match,something) && return parse(Int64,something)
-#    ismatch(float_match,something) && return parse(Float64,something)
-    try
-        return eval(parse(something))
-    catch
-        return something
-    end
-end
-
-randascii(n) = join([rand(vcat('A':'Z','a':'z')) for i=1:n])
 
 function timeout(expr,t::Integer)
     s = schedule(Task(expr))
@@ -73,9 +48,32 @@ function timeout(expr,t::Integer)
     end
 end
 
+# SIGNAL PROCESSING / STATS
+
+z{T<:Real,K<:Real,N}(p::AbstractArray{T,N}) = (p-mean(p))/std(p)
+
+polyparams{T<:Real}(x::AbstractArray{T,1},n::Integer) = [ float(x[i])^p for i = 1:length(x), p = 0:n ]
+polyfit{T<:Real}(y::AbstractArray{T,1},n::Integer) = polyparams(collect(1:length(y)),n) \ y
+polyfitline(len::Integer,params::Array{Float64,1}) = polyparams(collect(1:len),length(params)-1) * params''
+polyeval(x::Real,params::Array{Float64,1}) = [x^p for p=0:length(params)-1]' * params''
+
+# Conversion / String
+
+function smartparse(something)
+    try
+        return eval(parse(something))
+    catch
+        return something
+    end
+end
+
+randascii(n) = join([rand(vcat('A':'Z','a':'z')) for i=1:n])
+
 function objtodict(obj)
     Dict([(string(k),getfield(obj,k)) for k in fieldnames(obj)])
 end
+
+# List utilities
 
 function cluster(a,dist)
     sa = sort(a)
@@ -118,4 +116,8 @@ function getprecision(b)
     return minimum([abs(a[i]-a[i+1]) for i in 1:length(a)-1])
 end
 
-norepeat(a) = a[vcat(1,diff(a)) .!= 0]
+_isrepeat{T<:Real}(a::AbstractArray{T,1}) = vcat(1,diff(a)) .!= 0
+uniqueinds{T<:Real}(a::AbstractArray{T,1}) = find(_isrepeat(a))
+norepeat{T<:Real}(a::AbstractArray{T,1}) = a[_isrepeat(a)]
+
+end
